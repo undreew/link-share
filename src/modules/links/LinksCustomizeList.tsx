ad572d76d05e3834f3bd7c1d9c4b3f3aab80e9d3
james <jamesdejesus74@gmail.com>
2025-05-01 18:33:55 +0800

    fixed frickin overflow issue

--- src/modules/links/LinksCustomizeList.tsx
+++ src/modules/links/LinksCustomizeList.tsx
@@ -17,102 +17,102 @@ import { LinksPayload } from "@/types/payload";
 import { cn } from "@/lib/utils";
 
 interface Props {
-  data: Links;
-  onRemove: (id: number) => void;
-  onChange: Dispatch<SetStateAction<Links>>;
+	data: Links;
+	onRemove: (id: number) => void;
+	onChange: Dispatch<SetStateAction<Links>>;
 }
 
 const LinksCustomizeItem = ({
-  item,
-  onRemove,
+	item,
+	onRemove,
 }: {
-  item: {
-    id: number;
-    chosen: boolean;
-  };
-  onRemove: (id: number) => void;
+	item: {
+		id: number;
+		chosen: boolean;
+	};
+	onRemove: (id: number) => void;
 }) => {
-  const { control, formState } = useFormContext<LinksPayload>();
-  const { errors } = formState;
-  const { id, chosen } = item;
+	const { control, formState } = useFormContext<LinksPayload>();
+	const { errors } = formState;
+	const { id, chosen } = item;
 
-  return (
-    <div
-      className={cn("bg-gray-light p-4 cursor-grab", {
-        "cursor-grabbing": chosen,
-      })}
-    >
-      <div className="flex justify-between">
-        <div className="flex items-center gap-2">
-          <GripHorizontal size={15} className="text-gray-dark" />
-          <h1 className="text-[16px] font-bold text-gray-dark">Link# {id}</h1>
-        </div>
+	return (
+		<div
+			className={cn("bg-gray-light p-4 cursor-grab", {
+				"cursor-grabbing": chosen,
+			})}
+		>
+			<div className="flex justify-between">
+				<div className="flex items-center gap-2">
+					<GripHorizontal size={15} className="text-gray-dark" />
+					<h1 className="text-[16px] font-bold text-gray-dark">Link# {id}</h1>
+				</div>
 
-        <span className="body-md cursor-pointer" onClick={() => onRemove(id)}>
-          Remove
-        </span>
-      </div>
+				<span className="body-md cursor-pointer" onClick={() => onRemove(id)}>
+					Remove
+				</span>
+			</div>
 
-      <div className="mt-3 flex flex-col gap-3">
-        <div>
-          <FormField
-            control={control}
-            name={`links.${id}.platform`}
-            render={({ field }) => (
-              <FormItem>
-                <Label htmlFor="platform">Platform</Label>
-                <Select
-                  error={Boolean(errors?.links?.[id]?.platform?.message)}
-                  source={LINKS}
-                  {...field}
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+			<div className="mt-3 flex flex-col gap-3">
+				<div>
+					<FormField
+						control={control}
+						name={`links.${id}.platform`}
+						render={({ field }) => (
+							<FormItem>
+								<Label htmlFor="platform">Platform</Label>
+								<Select
+									error={Boolean(errors?.links?.[id]?.platform?.message)}
+									source={LINKS}
+									{...field}
+								/>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
 
-        <div>
-          <FormField
-            name={`links.${id}.link`}
-            control={control}
-            render={({ field }) => (
-              <FormItem>
-                <Label htmlFor="link">Link</Label>
-                <FormControl>
-                  <Input
-                    id="link"
-                    icon={<Link size={12} />}
-                    placeholder="e.g. alex@email.com"
-                    isInvalid={Boolean(errors?.links?.[item.id]?.link)}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-      </div>
-    </div>
-  );
+				<div>
+					<FormField
+						name={`links.${id}.link`}
+						control={control}
+						render={({ field }) => (
+							<FormItem>
+								<Label htmlFor="link">Link</Label>
+								<FormControl>
+									<Input
+										id="link"
+										icon={<Link size={12} />}
+										placeholder="e.g. alex@email.com"
+										isInvalid={Boolean(errors?.links?.[item.id]?.link)}
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const LinksCustomizeList: React.FC<Props> = ({ data, onChange, onRemove }) => {
-  return (
-    <ReactSortable
-      list={data}
-      animation={200}
-      setList={onChange}
-      className="flex flex-col gap-5 min-h-[345px] max-h-[345px] overflow-y-auto"
-    >
-      {map(data, (item) => {
-        return (
-          <LinksCustomizeItem key={item.id} item={item} onRemove={onRemove} />
-        );
-      })}
-    </ReactSortable>
-  );
+	return (
+		<ReactSortable
+			list={data}
+			animation={200}
+			setList={onChange}
+			className="relative flex flex-col gap-5 min-h-[345px] max-h-[345px] overflow-y-auto"
+		>
+			{map(data, (item) => {
+				return (
+					<LinksCustomizeItem key={item.id} item={item} onRemove={onRemove} />
+				);
+			})}
+		</ReactSortable>
+	);
 };
 
 export default LinksCustomizeList;
