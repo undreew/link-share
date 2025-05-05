14c279b1c1a9e64567319e687868fd6f936e796a
un.drew <82213003+undreew@users.noreply.github.com>
2025-05-05 12:22:13 +0800

    authentication added validation for create and confirm password

--- src/modules/register/RegisterFormFields.tsx
+++ src/modules/register/RegisterFormFields.tsx
@@ -9,70 +9,70 @@ import { FormItem, FormField } from "@/components/ui/form";
 import { FormMessage, FormControl } from "@/components/ui/form";
 
 const RegisterFormFields = () => {
-	const { control } = useFormContext<RegisterPayload>();
+  const { control } = useFormContext<RegisterPayload>();
 
-	return (
-		<div className="flex flex-col gap-5">
-			<FormField
-				name="email"
-				control={control}
-				render={({ field }) => (
-					<FormItem>
-						<Label htmlFor="email">Email</Label>
-						<FormControl>
-							<Input
-								id="email"
-								icon={<Mail size={12} />}
-								placeholder="e.g. alex@email.com"
-								{...field}
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
+  return (
+    <div className="flex flex-col gap-5">
+      <FormField
+        name="email"
+        control={control}
+        render={({ field }) => (
+          <FormItem>
+            <Label htmlFor="email">Email</Label>
+            <FormControl>
+              <Input
+                id="email"
+                icon={<Mail size={12} />}
+                placeholder="e.g. alex@email.com"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
 
-			<FormField
-				name="create_password"
-				control={control}
-				render={({ field }) => (
-					<FormItem>
-						<Label htmlFor="create_password">Password</Label>
-						<FormControl>
-							<Input
-								type="password"
-								id="create_password"
-								icon={<Lock size={12} />}
-								placeholder="At least 8 characters"
-								{...field}
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
+      <FormField
+        name="create_password"
+        control={control}
+        render={({ field }) => (
+          <FormItem>
+            <Label htmlFor="create_password">Password</Label>
+            <FormControl>
+              <Input
+                type="password"
+                id="create_password"
+                icon={<Lock size={12} />}
+                placeholder="At least 8 characters"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
 
-			<FormField
-				name="confirm_password"
-				control={control}
-				render={({ field }) => (
-					<FormItem>
-						<Label htmlFor="confirm_password">Password</Label>
-						<FormControl>
-							<Input
-								type="password"
-								id="confirm_password"
-								icon={<Lock size={12} />}
-								placeholder="At least 8 characters"
-								{...field}
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-		</div>
-	);
+      <FormField
+        name="confirm_password"
+        control={control}
+        render={({ field }) => (
+          <FormItem>
+            <Label htmlFor="confirm_password">Password</Label>
+            <FormControl>
+              <Input
+                type="password"
+                id="confirm_password"
+                icon={<Lock size={12} />}
+                placeholder="At least 8 characters"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
 };
 
 export default RegisterFormFields;
--- src/modules/register/useRegister.tsx
+++ src/modules/register/useRegister.tsx
@@ -1,36 +1,38 @@
-import { object, string } from "yup";
+import { object, ref, string } from "yup";
 import { useForm } from "react-hook-form";
 
 import { RegisterPayload } from "@/types/payload";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const useRegister = () => {
-	const validationSchema = object({
-		email: string()
-			.required("This field is required.")
-			.email("Must be a valid email address"),
-		create_password: string()
-			.min(8, "Password must be atleast 8 characters long.")
-			.required("This field is required."),
-		confirm_password: string()
-			.min(8)
-			.min(8, "Password must be atleast 8 characters long.")
-			.required("This field is required."),
-	});
+  const validationSchema = object({
+    email: string()
+      .required("This field is required.")
+      .email("Must be a valid email address"),
+    create_password: string()
+      .min(8, "Password must be atleast 8 characters long.")
+      .required("This field is required."),
+    confirm_password: string()
+      .min(8)
+      .min(8, "Password must be atleast 8 characters long.")
+      .required("This field is required.")
+      .oneOf([ref("create_password")], "Passwords do not match"),
+  });
 
-	const formValues = useForm<RegisterPayload>({
-		resolver: yupResolver(validationSchema),
-		defaultValues: { email: "", create_password: "", confirm_password: "" },
-	});
+  const formValues = useForm<RegisterPayload>({
+    mode: "onChange", // needed for the oneOf to work
+    resolver: yupResolver(validationSchema),
+    defaultValues: { email: "", create_password: "", confirm_password: "" },
+  });
 
-	function onSubmit(formData: RegisterPayload) {
-		console.log(formData);
-	}
+  function onSubmit(formData: RegisterPayload) {
+    console.log(formData);
+  }
 
-	return {
-		onSubmit,
-		formValues,
-	};
+  return {
+    onSubmit,
+    formValues,
+  };
 };
 
 export default useRegister;
